08da0a7f5f354a485af20536d3b6115d6f255b45
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2013-11-23 12:17:39 -0200

    Alterando a instalação do dotfiles

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
-dotfiles_basedir=$1
+path=$(pwd)
 
-for i in `find ${dotfiles_basedir}/dotfiles/ -maxdepth 1`; do
+mkdir -p $path/Configurations
+git clone git://github.com/jonathanccalixto/dotfiles.git $path/Configurations/dotfiles
+
+cd $HOME
+ln -s $path/Configurations/dotfiles
+
+dotfiles_basedir=$HOME/dotfiles
+
+for i in `find ${dotfiles_basedir}/dotfiles -maxdepth 1`; do
   echo $i;
   original_file=`basename $i`
   ln -snfv $i .${original_file}
 done
+
+ln -s dotfiles/bin/git-publish-branch bin/
+ln -s dotfiles/bin/campfire bin/
+ln -s dotfiles/vagrant.d .vagrant.d
