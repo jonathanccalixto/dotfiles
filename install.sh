4fbe16382a7aa8f958398610ba29c54ecbe15a4d
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-04-12 19:07:53 -0300

    [install.sh] Imports project when it doesn't exists

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 project="$HOME/Workspace/github/jonathanccalixto/dotfiles"
 dotfiles="$HOME/.dotfiles"
+
+# Imports project
+[[ ! -d "$project/.git" ]] && git clone https://github.com/jonathanccalixto/dotfiles.git $project
