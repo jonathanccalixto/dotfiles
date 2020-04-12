bc3644019f081e996d908a94fc74f07eca012131
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-04-12 19:40:15 -0300

    [install.sh] Makes shotcut to dotfiles folder in home path

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@ dotfiles="$HOME/.dotfiles"
 
 # Imports project
 [[ ! -d "$project/.git" ]] && git clone https://github.com/jonathanccalixto/dotfiles.git $project
+
+# Makes shotcut to dotfiles folder in home path
+[[ !d $dotfiles ]] && ln -s $project $dotfiles
+
