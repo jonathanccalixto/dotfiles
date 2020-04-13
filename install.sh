fd575a6f3907bd34d2dc3baa228fb01bb6c96619
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-04-13 17:28:04 -0300

    [install.sh] Fix variables

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ dotfiles="$HOME/.dotfiles"
 [[ ! -d "$project/.git" ]] && git clone https://github.com/jonathanccalixto/dotfiles.git $project
 
 # Makes shotcut to dotfiles folder in home path
-[[ !d $dotfiles ]] && ln -s $project $dotfiles
+[[ ! -d $dotfiles ]] && ln -s $project $dotfiles
 
 # Makes shortcut to dotfiles
-for original_file in `find ${dotfiles_basedir}/dotfiles.d -maxdepth 1`; do
+for original_file in `find ${dotfiles}/dotfiles.d -maxdepth 1`; do
   file=".`basename $original_file`"
   dotfile="$HOME/$file"
 
