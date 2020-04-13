266c9876e7e82dd98867848e1ce482a077c7f559
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-04-13 17:44:58 -0300

    [z.sh] Imports profiles files

--- z.sh
+++ z.sh
@@ -1,2 +1,6 @@
-# Applies spaceship themes
-ZSH_THEME="spaceship"
+# Imports profiles files
+for extension in (zsh sh); do
+  for file in `find ${dotfiles}/profile.d/*.$extension -n -maxdepth 1`; do
+    source $file;
+  done
+done
