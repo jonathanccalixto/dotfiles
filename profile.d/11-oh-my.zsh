455992c1fa03902eb1abc29fa178a1225cea93b3
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-04-14 12:33:42 -0300

    [11-oh-my.zsh] Configures zsh

---
+++ profile.d/11-oh-my.zsh
@@ -0,0 +1,8 @@
+# Defines oh-my-zsh plugins to use
+plugins=(git docker rbenv vi-mode rsync)
+
+# Defines oh-my-zsh path as zsh
+export ZSH="$HOME/.oh-my-zsh"
+
+# Imports oh-my-zsh configuration
+source $ZSH/oh-my-zsh.sh
