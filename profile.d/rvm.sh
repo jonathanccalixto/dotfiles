2494f7c37f325ce5a6bf6238c9bebee55ded694c
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-04-14 14:25:32 -0300

    [profile.d] Removes unused configurations

--- profile.d/rvm.sh
+++
@@ -1,5 +0,0 @@
-rvmrcit() {
-  # Create a gemset for the current directory (gem) name 
-  # under the currently selected (RVM) Ruby, optional.
-  rvm --create use "${rvm_ruby_string}@$(basename $(pwd))"
-}
