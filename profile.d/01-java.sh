d4f8265262d80fcd9b8b98c7e4e687f78aef33ce
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-04-14 14:46:11 -0300

    [profile.d] Defines java configurations

--- profile.d/01-java.sh
+++ profile.d/01-java.sh
@@ -6,19 +6,19 @@
 # JAVA_PROG_DIR=$HOME/programs/java/programs
 # JAVA_SERVERS_DIR=$HOME/programs/java/servers
 
-export   JAVA_HOME=$(/usr/libexec/java_home)
-# export    ANT_HOME=$JAVA_PROG_DIR/ant
-# export    MVN_HOME=$JAVA_PROG_DIR/maven
+# export JAVA_HOME=$(/usr/libexec/java_home)
+# export ANT_HOME=$JAVA_PROG_DIR/ant
+# export MVN_HOME=$JAVA_PROG_DIR/maven
 # export AWS_IAM_HOME=$JAVA_PROG_DIR/IAMCli
 # export GRADLE_HOME=$JAVA_PROG_DIR/gradle
 
-# export    ROO_HOME=$JAVA_PROG_DIR/spring-roo
-# export  SCALA_HOME=$JAVA_LANG_DIR/scala
+# export ROO_HOME=$JAVA_PROG_DIR/spring-roo
+# export SCALA_HOME=$JAVA_LANG_DIR/scala
 # export GROOVY_HOME=$JAVA_LANG_DIR/groovy
 # export GRAILS_HOME=$JAVA_PROG_DIR/grails
-# #export TOMCAT_HOME=$JAVA_SERVERS_DIR/tomcat
+# # export TOMCAT_HOME=$JAVA_SERVERS_DIR/tomcat
 
-export PATH=$JAVA_HOME/bin:$PATH
+# export PATH=$JAVA_HOME/bin:$PATH
 # export PATH=$ANT_HOME/bin:$PATH
 # export PATH=$MVN_HOME/bin:$PATH
 # export PATH=$AWS_IAM_HOME/bin:$PATH
