7ab31ddbb585c8a2a30ae6b8a660356157f5bc75
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-04-22 18:54:22 -0300

    Ignore changes at git-custom.sh

--- profile.d/02-git-custom.sh
+++
@@ -1,5 +0,0 @@
-git config --global alias.ci 'commit -S'
-git config --global alias.amend "commit -S --amend"
-git config --global user.name "Jonathan C. Calixto"
-git config --global user.email jonathanccalixto@gmail.com
-git config --global user.signingkey "3C15F404B871867F"
