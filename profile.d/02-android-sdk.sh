30f3fdaffc81cae0268d8307c057ebb2b14aa176
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-10-24 10:17:40 -0300

    [profile.d] Android SDK configured

---
+++ profile.d/02-android-sdk.sh
@@ -0,0 +1,9 @@
+ANDROID_SDK=$HOME/Android/Sdk
+SDK_28="$ANDROID_SDK/build-tools/28.0.3"
+SDK_29="$ANDROID_SDK/build-tools/29.0.3"
+SDK_30="$ANDROID_SDK/build-tools/30.0.2"
+
+# Defines my executables
+[ -d $SDK_28 ] && export PATH="$SDK_28:$PATH"
+[ -d $SDK_29 ] && export PATH="$SDK_29:$PATH"
+[ -d $SDK_30 ] && export PATH="$SDK_30:$PATH"
