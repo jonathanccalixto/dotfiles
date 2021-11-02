56c2217919fda6fd1166cec65ef3e6777e9b0e86
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2021-11-01 22:24:30 -0300

    [02-yarn.sh] ignore yarn global bin path

--- profile.d/02-yarn.sh
+++ profile.d/02-yarn.sh
@@ -6,6 +6,6 @@ fi
 
 if [ -x "$(which npm)" ]; then
   [[ ! -x "$(which yarn)" ]] && sh -c "npm install yarn -g; clear" # Installs yarn
-  export PATH="$(yarn global bin):$PATH" # Adds yarn global to $PATH
+  # export PATH="$(yarn global bin):$PATH" # Adds yarn global to $PATH
   export PATH="$(npm -g bin):$PATH" # Adds npm global to $PATH
 fi
