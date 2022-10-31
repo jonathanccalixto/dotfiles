688857264222177f3a878c40a43b23109f808737
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2022-10-30 21:53:05 -0300

    [profile.d/02-android-sdk.sh] Update SDK PATH

--- profile.d/02-android-sdk.sh
+++ profile.d/02-android-sdk.sh
@@ -2,8 +2,10 @@ ANDROID_SDK=$HOME/Android/Sdk
 SDK_30="$ANDROID_SDK/build-tools/30.0.3"
 SDK_31="$ANDROID_SDK/build-tools/31.0.0"
 SDK_32="$ANDROID_SDK/build-tools/32.0.0"
+SDK_33="$ANDROID_SDK/build-tools/33.0.0"
 
 # Defines my executables
-[ -d $SDK_30 ] && export PATH="$SDK_30:$PATH"
-[ -d $SDK_31 ] && export PATH="$SDK_31:$PATH"
-[ -d $SDK_32 ] && export PATH="$SDK_32:$PATH"
+# [ -d $SDK_30 ] && export PATH="$SDK_30:$PATH"
+# [ -d $SDK_31 ] && export PATH="$SDK_31:$PATH"
+# [ -d $SDK_32 ] && export PATH="$SDK_32:$PATH"
+[ -d $SDK_33 ] && export PATH="$SDK_33:$PATH"
