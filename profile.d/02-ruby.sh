b2f1fb6001cca13fc940f824e5df37e38079f02b
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2022-10-30 21:55:12 -0300

    [profile.d/02-ruby] just ruby configuration to execute more fast

--- profile.d/02-ruby.sh
+++ profile.d/02-ruby.sh
@@ -1,5 +1,6 @@
 if [[ $MY_SO = 'MacOS' ]]; then
-  export ruby_configure_flags="--with-readline-dir=$(brew info readline | grep -i cellar | cut -d ' ' -f 1)"
+  # export ruby_configure_flags="--with-readline-dir=$(brew info readline | grep -i cellar | cut -d ' ' -f 1)" &
+  export ruby_configure_flags="--with-readline-dir=$($(which ruby) -e "puts  Dir['/usr/local/Cellar/readline/*'].sort.last")"
 elif [[ $MY_SO = 'Linux' ]]; then
   export ruby_configure_flags="--with-readline-dir=/usr/include/readline"
 fi
