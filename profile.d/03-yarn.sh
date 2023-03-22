6aa2374278eba80cf8400d00f7b41a53665ef6b7
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2023-03-22 13:28:58 -0300

    [03-yarn.sh] install @socketsecurity plugin

--- profile.d/03-yarn.sh
+++ profile.d/03-yarn.sh
@@ -4,8 +4,14 @@ if [[ $MY_SO -eq 'MacOS' ]]; then
   fi
 fi
 
-if [ -x "$(which npm)" ]; then
-  [[ ! -x "$(which yarn)" ]] && sh -c "npm install --location=global yarn; clear" # Installs yarn
+if [[ -x "$(which npm)" ]]; then
+  [[ ! -x "$(which socket)" ]] && sh -c "npm i -g @socketsecurity/cli" # Installs socket
+
+  # alias npm="socket npm"
+
+  [[ ! -x "$(which yarn)" ]] && sh -c "npm i -g yarn@latest" # Installs yarn
   # export PATH="$(yarn global bin):$PATH" # Adds yarn global to $PATH
-  export PATH="$(npm --location=global bin):$PATH" # Adds npm global to $PATH
+  # export PATH="$(npm root -g)/npm/bin:$PATH" # Adds npm global to $PATH
+else
+  echo "NPM not found"
 fi
