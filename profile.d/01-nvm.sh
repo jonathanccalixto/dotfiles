0b9b94151b70ad857e0f01cddb4842618ef35f5d
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2023-03-22 13:24:12 -0300

    [01-nvm.sh] Autoload and auto install node version through nvm

--- profile.d/01-nvm.sh
+++ profile.d/01-nvm.sh
@@ -6,3 +6,17 @@ export NVM_DIR="$HOME/.nvm"
 
 # Imports nvm completion
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+if [[ -f "$PWD/.node-version" ]]; then
+  NODE_VERSION=$(cat $PWD/.node-version | sed "s|v||")
+elif [[ -f "$PWD/.nvmrc" ]]; then
+  NODE_VERSION=$(cat $PWD/.nvmrc | sed "s|v||")
+fi
+
+if [[ "$NODE_VERSION" ]]; then
+  [[ ! "$(nvm list | grep $NODE_VERSION | cut -d"v" -f2)" ]] && nvm install $NODE_VERSION
+
+  nvm use $NODE_VERSION
+else
+  echo "node version not found"
+fi
