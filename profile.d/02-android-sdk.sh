56e7e911261a690b23295f57fd1f1ed4d0a7ae97
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2023-07-17 14:49:38 -0300

    [02-android-sdk.sh] add android sdk 34.0.0

--- profile.d/02-android-sdk.sh
+++ profile.d/02-android-sdk.sh
@@ -3,9 +3,11 @@ SDK_30="$ANDROID_SDK/build-tools/30.0.3"
 SDK_31="$ANDROID_SDK/build-tools/31.0.0"
 SDK_32="$ANDROID_SDK/build-tools/32.0.0"
 SDK_33="$ANDROID_SDK/build-tools/33.0.0"
+SDK_33="$ANDROID_SDK/build-tools/34.0.0"
 
 # Defines my executables
 # [ -d $SDK_30 ] && export PATH="$SDK_30:$PATH"
 # [ -d $SDK_31 ] && export PATH="$SDK_31:$PATH"
 # [ -d $SDK_32 ] && export PATH="$SDK_32:$PATH"
+# [ -d $SDK_33 ] && export PATH="$SDK_33:$PATH"
 [ -d $SDK_33 ] && export PATH="$SDK_33:$PATH"
