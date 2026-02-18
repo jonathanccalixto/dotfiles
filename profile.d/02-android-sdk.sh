c1e3f66f9bb14814cced231154dc3bc64a163164
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2026-02-18 08:44:39 -0300

    [01-java.sh] adjust ANDROID_HOME

--- profile.d/02-android-sdk.sh
+++ profile.d/02-android-sdk.sh
@@ -5,15 +5,8 @@ export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 
-SDK_30="$ANDROID_HOME/build-tools/30.0.3"
-SDK_31="$ANDROID_HOME/build-tools/31.0.0"
-SDK_32="$ANDROID_HOME/build-tools/32.0.0"
-SDK_33="$ANDROID_HOME/build-tools/33.0.2"
 SDK_34="$ANDROID_HOME/build-tools/34.0.0"
+SDK_35="$ANDROID_HOME/build-tools/35.0.1"
 
 # Defines my executables
-[ -d $SDK_30 ] && export PATH="$SDK_30:$PATH"
-[ -d $SDK_31 ] && export PATH="$SDK_31:$PATH"
-[ -d $SDK_32 ] && export PATH="$SDK_32:$PATH"
-[ -d $SDK_33 ] && export PATH="$SDK_33:$PATH"
-[ -d $SDK_34 ] && export PATH="$SDK_34:$PATH"
+[ -d $SDK_35 ] && export PATH="$SDK_35:$PATH"
